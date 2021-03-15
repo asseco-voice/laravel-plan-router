08042ef07e1c2e72f00916953de52ee1ca265ae7
Marko Dupor <marko.dupor@gmail.com>
2021-03-15 14:42:17 +0100

    Service provider fixes

--- src/PlanRouterServiceProvider.php
+++ src/PlanRouterServiceProvider.php
@@ -13,7 +13,7 @@ class PlanRouterServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/asseco-remote-relations.php', 'asseco-remote-relations');
+        $this->mergeConfigFrom(__DIR__ . '/../config/asseco-plan-router.php', 'asseco-plan-router');
         $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
 
         if (config('asseco-containers.runs_migrations')) {
@@ -26,14 +26,8 @@ class PlanRouterServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $timestamp = now()->format('Y_m_d_His');
-
         $this->publishes([
-            __DIR__ . config('asseco-remote-relations.stub_path') => database_path("migrations/{$timestamp}_create_remote_relations_table.php"),
-        ], 'asseco-remote-relations');
-
-        $this->publishes([
-            __DIR__ . '/../config/asseco-remote-relations.php' => config_path('asseco-remote-relations.php'),
-        ], 'asseco-remote-relations');
+            __DIR__ . '/../config/asseco-plan-router.php' => config_path('asseco-plan-router.php'),
+        ], 'asseco-plan-router');
     }
 }
